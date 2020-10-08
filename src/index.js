61c5f76c3df2948370cde703994cfaf4af936ee0
Ing. Nelson Gonzalez <nescalan@gmail.com>
2020-10-08 16:19:45 -0600

    Nuevo Componente: Badge.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import Badge from './components/Badge'
+
 import './global.css';
 import App from './components/App';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badge />, container);

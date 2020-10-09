1bb985c556ee7cb2133c2a5391acbc68aa0cf0b7
Ing. Nelson Gonzalez <nescalan@gmail.com>
2020-10-09 10:45:58 -0600

    Nuevo Componente Llamado Badge.js

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import confLogo from '../images/logo.svg';
+import './styles/Badge.css';
 
 class Badge extends React.Component {
     render(){
         return(
-            <h1>Hello</h1>
+        <div className="Badge">
+            <div className="Badge__header">
+              <img src={confLogo} alt="Logo de la Conferencia"/>
+            </div>
+
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://avatars1.githubusercontent.com/u/30724303?s=460&u=dfb8b4d0c6630a4729937d9dfe2c3b34ba346b6a&v=4" alt="Avatar"/>
+                <h1>Ing. Nelson <br/> Gonzalez</h1>
+            </div>
+
+            <div className="Badge__section-info">
+                <h3>Frontend Engineer</h3> 
+                <div>@mulurkie</div>
+            </div>
+
+            <div className="Badge__footer">
+                #platziconf
+            </div>
+
+        </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-
-import Badge from './components/Badge'
-
 import './global.css';
+
+import Badge from './components/Badge';
 import App from './components/App';
 
 const container = document.getElementById('app');

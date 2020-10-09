9882ea6faee8e4f4a2b1828dc58bf126ef9af4ea
Ing. Nelson Gonzalez <nescalan@gmail.com>
2020-10-09 14:56:51 -0600

    14: Enlazando Estados

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 
 class BadgeForm extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    handleChange = (e) => {
+        console.log({
+            name: e.target.name,
+            value: e.target.value
+        })
+    }
+
+    handleClick = (e) => {
+        console.log("Button Was Clicked")
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Was Submited")
+    }
+
+    render() {
+        return (
             <div>
 
                 <h1>New Attendant</h1>
 
-                <form action=""></form>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Nombre Completo</label>
+                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+                        <button onClick={this.handleClick} className="btn btn-primary">Save</button>
+                    </div>
+                </form>
             </div>
-         );
+        );
     }
 }
- 
+
 export default BadgeForm;

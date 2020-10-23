ebb837c24b04fc214024726567d7e488a1bbec9e
Ing. Nelson G.E <nescalan@gmail.com>
2020-10-23 15:04:59 -0600

    17: BadgeList Terminiado

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,20 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 
-class BadgesList extends Component {
-  state = {};
+import "./styles/BadgesList.css";
+
+class BadgesListItem extends React.Component {
   render() {
     return (
-      <ul className="list-unstyled">
-        {this.props.Badges.map((Badge) => {
-          return (
-            <li key={Badge.id}>
-              <p>
-                {Badge.firstName} {Badge.lastName}
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.avatarUrl}
+          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+        />
+
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <br />@{this.props.badge.twitter}
+          <br />
+          {this.props.badge.jobTitle}
+        </div>
+      </div>
+    );
+  }
+}
+
+class BadgesList extends React.Component {
+  render() {
+    return (
+      <div className="BadgesList">
+        <ul className="list-unstyled">
+          {this.props.badges.map((badge) => {
+            return (
+              <li key={badge.id}>
+                <BadgesListItem badge={badge} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import BadgeNew from "./pages/BadgeNew";
+// import BadgeNew from "./pages/BadgeNew";
 import Badges from "./pages/Badges";
 
 const container = document.getElementById("app");

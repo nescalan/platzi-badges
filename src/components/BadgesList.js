3d076d0061aa35bab1a37a7b819eb75b20dcd2c8
Ing. Nelson G.E <nescalan@gmail.com>
2020-10-23 15:09:47 -0600

    17: BadgesListCorregido

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,44 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-import "./styles/BadgesList.css";
-
-class BadgesListItem extends React.Component {
+class BadgesList extends Component {
+  state = {};
   render() {
     return (
-      <div className="BadgesListItem">
-        <img
-          className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
-        />
-
-        <div>
-          <strong>
-            {this.props.badge.firstName} {this.props.badge.lastName}
-          </strong>
-          <br />@{this.props.badge.twitter}
-          <br />
-          {this.props.badge.jobTitle}
-        </div>
-      </div>
-    );
-  }
-}
-
-class BadgesList extends React.Component {
-  render() {
-    return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {this.props.badges.map((badge) => {
-            return (
-              <li key={badge.id}>
-                <BadgesListItem badge={badge} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <ul className="list-unstyled">
+        {this.props.Badges.map((Badge) => {
+          return (
+            <li key={Badge.id}>
+              <p>
+                {Badge.firstName} {Badge.lastName}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
